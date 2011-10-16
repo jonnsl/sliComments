jonnsl/sliComments

commit a86d82eb27164c36aafa1f3b40331ee4c82f3eb3

Add Option to select whether guests can comment.

components/com_slicomments/views/comments/tmpl/default_form.php
@@ -2,6 +2,7 @@
 // no direct access
 defined('_JEXEC') or die;
 $user = JFactory::getUser();
+$guestComment = (bool)$this->params->get('guest', true);
 JHtml::_('script', 'slicomments/comments_form.min.js', true, true);
 ?>
 <div class="comments_form no-js">
@@ -10,7 +11,7 @@ JHtml::_('script', 'slicomments/comments_form.min.js', true, true);
 		<input type="hidden" name="return" value="<?php echo base64_encode(JFactory::getURI()->toString()); ?>"/>
 		<input type="hidden" name="article_id" value="<?php echo JRequest::getInt('id'); ?>"/>
 		<ul class="comments_form_inputs">
-			<?php if ($user->guest): ?>
+			<?php if ($guestComment && $user->guest): ?>
 			<li>
 				<label><?php echo JText::_('COM_COMMENTS_LABEL_NAME'); ?></label>
 				<input id="comments_form_name" type="text" placeholder="<?php echo JText::_('COM_COMMENTS_LABEL_NAME'); ?>" name="name" class="required validation-failed"/>
@@ -19,20 +20,28 @@ JHtml::_('script', 'slicomments/comments_form.min.js', true, true);
 				<label><?php echo JText::_('COM_COMMENTS_LABEL_EMAIL'); ?></label>
 				<input id="comments_form_email" type="text" placeholder="<?php echo JText::_('COM_COMMENTS_LABEL_EMAIL'); ?>" name="email" class="validate-email"/>
 			</li>
-			<?php else: ?>
-				<input id="comments_form_name" type="hidden" name="name" value="<?php echo $user->name; ?>"/>
-				<input id="comments_form_email" type="hidden" name="email" value="<?php echo $user->email; ?>"/>
 			<?php endif; ?>
 			<li>
+				<?php if (!$guestComment && $user->guest): ?>
+				<label class="login-to-post" for="comments_form_textarea">
+					<?php echo preg_replace('/#([^#]*)#/i', '<a href="'.JRoute::_('index.php?option=com_users&view=login').'">$1</a>', JText::_('COM_COMMENTS_LOGIN_TO_POST_COMMENT'), 1); ?>
+				</label>
+				<?php else: ?>
 				<label for="comments_form_textarea"><?php echo JText::_('COM_COMMENTS_LABEL_TEXT'); ?></label>
-				<textarea id="comments_form_textarea" name="text" placeholder="<?php echo JText::_('COM_COMMENTS_LABEL_TEXT'); ?>" class="required minLength:5"></textarea>
+				<?php endif; ?>
+				<textarea id="comments_form_textarea" name="text" class="required minLength:5"
+				<?php if (!$guestComment && $user->guest) {echo ' disabled';} else {echo 'placeholder="'.JText::_('COM_COMMENTS_LABEL_TEXT').'"';} ?>></textarea>
 			</li>
 		</ul>
+		<?php if (!(!$guestComment && $user->guest)): ?>
 		<p class="comments-remaining">
 			<?php echo JText::sprintf('COM_COMMENTS_CHARACTERS_REMAINING', '<span id="comments-remaining-count">500</span>'); ?>
 		</p>
+		<?php endif; ?>
 		<p class="comments-post-buttons">
-			<button type="submit" id="comments_form_send"><?php echo JText::_('COM_COMMENTS_SUBMIT'); ?></button>
+			<button type="submit" id="comments_form_send" <?php if (!$guestComment && $user->guest) echo ' disabled';?>>
+				<?php echo JText::_('COM_COMMENTS_SUBMIT'); ?>
+			</button>
 		</p>
 	</form>
 </div>

media/slicomments/js/comments_form.js
@@ -40,7 +40,7 @@ window.addEvent('domready', function(){
 	})()
 	if (placeholder_support) {
 		// Hide the labels
-		$$('.comments_form_inputs li label').setStyle('display', 'none');
+		if (!textarea.get('disabled')) $$('.comments_form_inputs li label').setStyle('display', 'none');
 	}
 	else {
 		// Use OverText to simulate placeholders

administrator/components/com_slicomments/config.xml
@@ -2,13 +2,23 @@
 <config>
 	<fieldset name="BASIC">
 		<field
+			name="guest"
+			type="radio"
+			label="COM_COMMENTS_ENABLE_GUEST_LABEL"
+			description="COM_COMMENTS_ENABLE_GUEST_DESC"
+			default="1">
+				<option value="1">JYES</option>
+				<option value="0">JNO</option>
+		</field>
+
+		<field
 			name="ratings"
-			type="list"
+			type="radio"
 			label="COM_COMMENTS_ENABLE_RATINGS_LABEL"
 			description="COM_COMMENTS_ENABLE_RATINGS_DESC"
 			default="1">
-				<option value="0">JNO</option>
 				<option value="1">JYES</option>
+				<option value="0">JNO</option>
 		</field>
 
 		<field

administrator/language/en-GB/en-GB.com_slicomments.ini
@@ -22,3 +22,5 @@ COM_COMMENTS_CATEGORY_INCLUDE_CHILD_LABEL="Child Category Articles"
 COM_COMMENTS_CATEGORY_INCLUDE_CHILD_DESC="Include or Exclude Articles from Child Categories."
 COM_COMMENTS_CATEGORY_OPTION_INCLUDE_VALUE="Include"
 COM_COMMENTS_CATEGORY_OPTION_EXCLUDE_VALUE="Exclude"
+COM_COMMENTS_ENABLE_GUEST_LABEL="Guest comments"
+COM_COMMENTS_ENABLE_GUEST_DESC="Select 'Yes' if you want that not logged-in users to be able to comment."

components/com_slicomments/models/comments.php
@@ -6,6 +6,12 @@ jimport('joomla.application.component.modellist');
 
 class sliCommentsModelComments extends JModelList
 {
+	public function __construct($config = array())
+	{
+		$this->params = JComponentHelper::getParams('com_slicomments');
+		return parent::__construct($config);
+	}
+
 	public function filter($data)
 	{
 		$user = JFactory::getUser();
@@ -28,6 +34,11 @@ class sliCommentsModelComments extends JModelList
 
 	public function validate($data)
 	{
+		if (!$this->params->get('guest', true) && $data['user_id'] == 0) {
+			$this->setError(JText::_('COM_COMMENTS_ERROR_LOGIN_TO_POST_COMMENT'));
+			return false;
+		}
+
 		$db = $this->_db;
 		$query = $db->getQuery(true)
 			->select('catid')
@@ -60,11 +71,10 @@ class sliCommentsModelComments extends JModelList
 
 	public function isCategoryEnabled($id)
 	{
-		$params = JComponentHelper::getParams('com_slicomments');
-		$catids = $params->get('catid');
+		$catids = $this->params->get('catid');
 		if ($catids[0])
 		{
-			if ($params->get('include_child'))
+			if ($this->params->get('include_child'))
 			{
 				jimport('joomla.application.categories');
 				JModel::addIncludePath(JPATH_SITE.'/components/com_content/models', 'ContentModel');

language/en-GB/en-GB.com_slicomments.ini
@@ -17,6 +17,8 @@ COM_COMMENTS_ERROR_COULD_NOT_STORE_VOTE_DEBUG="Could not store vote in the datab
 COM_COMMENTS_ERROR_COULD_NOT_STORE_VOTE="Error voting."
 COM_COMMENTS_ERROR_COULD_NOT_UPDATE_VOTE_CACHE="Could not update the rating cache: %s"
 COM_COMMENTS_ERROR_RATINGS_DISABLED="Ratings are disabled."
+COM_COMMENTS_ERROR_LOGIN_TO_POST_COMMENT="Log-in to post a comment!"
 COM_COMMENTS_ACTION_LIKE="Like"
 COM_COMMENTS_ACTION_DISLIKE="Dislike"
 COM_COMMENTS_ACTION_DELETE="Delete"
+COM_COMMENTS_LOGIN_TO_POST_COMMENT="#Log-in# to post a comment!"

media/slicomments/css/style.css
@@ -102,6 +102,14 @@
 .overTxtLabel {
 	color: #999
 }
+.login-to-post{
+	line-height: normal;
+	cursor: text;
+	position: absolute;
+	left: 6px;
+	top: 6px;
+	color: #333
+}
 #comments_section .pagination {
 	border-top: none !important
 }

commit 59262ddb93359259fbd4962cd383bab5c7cccdfb

Fixed bug introduced by a86d82eb27 . Comments were not being saved.

components/com_slicomments/views/comments/tmpl/default_form.php
@@ -6,7 +6,7 @@ $guestComment = (bool)$this->params->get('guest', true);
 JHtml::_('script', 'slicomments/comments_form.min.js', true, true);
 ?>
 <div class="comments_form no-js">
-	<form id="comments_form" action="<?php echo JRoute::_('index.php?option=com_slicomments&task=comments.post'); ?>" method="post">
+	<form id="comments_form" action="<?php echo JRoute::_('index.php?option=com_slicomments&task=comments.post'); ?>" method="post" data-logged="<?php echo (!$user->guest ? '1' : '0');?>">
 		<?php echo JHtml::_('form.token'); ?>
 		<input type="hidden" name="return" value="<?php echo base64_encode(JFactory::getURI()->toString()); ?>"/>
 		<input type="hidden" name="article_id" value="<?php echo JRequest::getInt('id'); ?>"/>

media/slicomments/js/comments_form.js
@@ -5,6 +5,7 @@ window.addEvent('domready', function(){
 	var textarea = $('comments_form_textarea');
 	var counter = $('comments-remaining-count');
 	var form = $('comments_form');
+	var logged = form.get('data-logged') == 1 ? true : false;
 	var validator = new Form.Validator.Inline(form, {
 		scrollToErrorsOnSubmit: false,
 		evaluateFieldsOnChange: false,
@@ -87,8 +88,10 @@ window.addEvent('domready', function(){
 				}
 			}
 		});
-		new OverText($('comments_form_name'));
-		new OverText($('comments_form_email'));
+		if (!logged) {
+			new OverText($('comments_form_name'));
+			new OverText($('comments_form_email'));
+		}
 	}
 	
 	$('comments_form_send').addEvent('click', function(e){
@@ -97,7 +100,7 @@ window.addEvent('domready', function(){
 			new Request.JSON({
 				url: form.get('action')+'&format=json',
 				method: 'post',
-				data: 'article_id='+form.article_id.value+'&name='+form.name.value+'&email='+form.email.value+'&text='+form.text.value+'&'+form.getElement('input[type=hidden]').get('name')+'=1',
+				data: 'article_id='+form.article_id.value+(!logged ? '&name='+form.name.value+'&email='+form.email.value : '')+'&text='+form.text.value+'&'+form.getElement('input[type=hidden]').get('name')+'=1',
 				onSuccess: function(response){
 					if (response.success) {
 						var data = response.data;