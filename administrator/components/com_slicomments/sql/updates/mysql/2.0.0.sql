6e4f6a7dbf3e7776a831a51ade77fbf82918a9f2
Jonnathan <jonnsl@hotmail.com>
2013-03-17 01:02:20 -0300

    Add update sql file.

---
+++ administrator/components/com_slicomments/sql/updates/mysql/2.0.0.sql
@@ -0,0 +1,8 @@
+ALTER TABLE  `#__slicomments` ADD `item_id` integer unsigned NOT NULL,
+ADD `extension` varchar(50) NOT NULL;
+
+UPDATE `#__slicomments` SET
+item_id = article_id,
+extension = "com_content";
+
+ALTER TABLE `jos_slicomments` DROP `article_id`;
