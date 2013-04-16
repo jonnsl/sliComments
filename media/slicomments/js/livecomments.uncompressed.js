8a17598be36da9aee979617fdc5a22ff30959783
Jonnathan <jonnsl@hotmail.com>
2013-04-16 20:52:41 -0300

    Refactor.

--- media/slicomments/js/livecomments.uncompressed.js
+++ media/slicomments/js/livecomments.uncompressed.js
@@ -8,120 +8,142 @@ window.addEvent('domready', function(){
 	var tmpl = require('./tmpl_comment.js'),
 		comments_list = $('comments_list'),
 		comments_counter = $('comments_counter'),
+		liveCommentsDisable = $('live-comments-disable'),
+		liveCommentsInfo = $('live-comments-info'),
 		latestComment = comments_list.getFirst(),
 		lastCommentTime = latestComment ? latestComment.getElement('.metadata .created').get('data-created') : 0,
 		queue = [],
 		_slice = [].slice,
 		_push = [].push,
-
-		request = new Request({
-			url: 'index.php?option=com_slicomments&task=comments.live&item_id='+form.item_id.get('value')+'&extension='+form.extension.get('value'),
-			format: 'raw',
-			method: 'get',
-			noCache: true,
-			onSuccess: function(res) {
-				if (request.status == 200) {
-					var comments = JSON.decode(res),
-						temp = new Element('ul', {
-							html: comments.map(function(comment) {
-								return tmpl.call(sliComments, comment);
-							}).join('')
-						});
-					// add new comments to the end of the queue
-					_push.apply(queue, _slice.call(temp.getChildren('li.comment')));
-					lastCommentTime = comments[comments.length-1].created;
-					if (autoUpdate) {
-						showComments();
-					} else {
-						liveCommentsInfo.show();
-						liveCommentsCounter.set('html', (isRunning ? comments : queue).length.toString());
-					}
-				}
-			},
-			onFailure: function(xhr){
-				if (xhr.status == 400){
-					console.log(xhr.responseText);
-				}
-				// Don't try again
-				clearInterval(requestTimer);
-			}
-		}),
-
-		cc = comments_counter.get('html'), // Total number of comments
-		liveCommentsDisable = $('live-comments-disable'),
-		liveCommentsInfo = $('live-comments-info'),
+		cc = comments_counter.get('html'),
 		liveCommentsCounter = liveCommentsInfo.getElement('strong'),
 		autoUpdate = false,
 		isRunning = false;
 
-
-		/**
-		 * Inject in the list the latest comments necessary to fill a page.
-		 * Activated when the user click on "show them"
-		 */
-		var l = 0;
-		function showComments(e){
-			if (e){
-				e.stop();
-				liveCommentsInfo.dissolve();
-			}
-			if (check()) return;
-			isRunning = true;
-			if (!l) l = queue.length;
-			// Inject the comments in the list, one at a time, with a interval of half a second between them.
-			var timer = (function(){
-				if (l == 0) {
-					isRunning = false;
-					clearInterval(timer);
-					return;
+	var request = new Request({
+		url: 'index.php?option=com_slicomments&task=comments.live&item_id='+form.item_id.get('value')+'&extension='+form.extension.get('value'),
+		format: 'raw',
+		method: 'get',
+		noCache: true,
+		onSuccess: function(res){
+			if (request.status == 200) {
+				var comments = JSON.decode(res),
+					temp = new Element('ul', {
+						html: comments.map(function(comment){
+							return tmpl.call(sliComments, comment);
+						}).join('')
+					});
+				// add new comments to the end of the queue
+				_push.apply(queue, _slice.call(temp.getChildren('li.comment')));
+				lastCommentTime = comments[comments.length-1].created;
+				if (autoUpdate) {
+					showComments();
+				} else {
+					liveCommentsInfo.show();
+					liveCommentsCounter.set('html', (isRunning ? comments : queue).length.toString());
 				}
+			}
+		},
+		onFailure: function(xhr){
+			if (xhr.status == 400) {
+				console.log(xhr.responseText);
+			}
+			// Don't try again
+			clearInterval(requestTimer);
+		}
+	});
+
+	/**
+	 * The number of comments that will be removed from the queue.
+	 *
+	 * @type {number}
+	 */
+	var l = 0;
+
+	/**
+	 * Show the comments currently in the queue
+	 *
+	 * @param {Event=} e
+	 */
+	function showComments(e){
+		if (e) {
+			e.stop();
+			liveCommentsInfo.dissolve();
+		}
+		if (check()) return;
+		isRunning = true;
+		if (!l) l = queue.length;
+		var timer = (function(){
+			if (l == 0) {
+				isRunning = false;
+				clearInterval(timer);
+			} else {
 				l--;
 				insertComment(queue.shift());
-			}).periodical(500);
-		};
+			}
+		}).periodical(500);
+	};
 
-		/**
-		 * Check if showComments is running, if it's, update the size of the queue that need to be shown.
-		 */
-		function check(){
-			if (!isRunning) return false;
+	/**
+	 * Check if showComments is running.
+	 * if it's, update the size of the queue that need to be shown.
+	 *
+	 * @return {boolean}
+	 */
+	function check(){
+		if (isRunning) {
 			l = queue.length;
-			return true;
-		};
+		}
+		return isRunning;
+	};
 
-		function insertComment(comment){
-			comments_counter.set('text', ++cc)
-			comment.setStyle('opacity', 0)
-				.inject(comments_list, 'top')
-				.fade();
-			comments_list.getLast().nix(true);
-		};
+	/**
+	 * Insert a comment in the comments list
+	 *
+	 * @param {Element} comment The comment to be inserted
+	 */
+	function insertComment(comment){
+		comments_counter.set('text', ++cc)
+		comment.setStyle('opacity', 0)
+			.inject(comments_list, 'top')
+			.fade();
+		comments_list.getLast().nix(true);
+	};
 
-		/**
-		 * Activate the auto update
-		 */
-		function activateAutoUpdate(e){
-			autoUpdate = true;
-			showComments(e);
-			liveCommentsDisable.show();
-		};
+	/**
+	 * Activate the auto update
+	 *
+	 * @param {Event} e
+	 */
+	function activateAutoUpdate(e){
+		autoUpdate = true;
+		showComments(e);
+		liveCommentsDisable.show();
+	};
 
-		/**
-		 * dectivate the auto update
-		 */
-		function disableAutoUpdate(e){
-			e.stop();
-			liveCommentsDisable.hide();
-			autoUpdate = false;
-		};
+	/**
+	 * dectivate the auto update
+	 *
+	 * @param {Event} e
+	 */
+	function disableAutoUpdate(e){
+		e.stop();
+		liveCommentsDisable.hide();
+		autoUpdate = false;
+	};
 
-		var requestTimer = (function(){
-			request.send({
-				data: {
-					lt: lastCommentTime
-				}
-			});
-		}).periodical(3000);
+	/**
+	 * Check for new comments every 3sec
+	 *
+	 * @type {number} IntervalID
+	 */
+	var requestTimer = (function(){
+		request.send({
+			data: {
+				lt: lastCommentTime
+			}
+		});
+	}).periodical(3000);
 
 	// Add events
 	liveCommentsDisable.getElement('a').addEvent('click', disableAutoUpdate);
